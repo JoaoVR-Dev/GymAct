f257b866615dcaed46002786bdadd44501f1e89d
JoaoVR-Dev <joaovr.dev@outlook.com>
2023-10-13 14:39:11 -0300

    Adicionando aplicação ao repositorio

---
+++ GymAct/src/main/java/com/example/GymAct/GymActApplication.java
@@ -0,0 +1,13 @@
+package com.example.GymAct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymActApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymActApplication.class, args);
+	}
+
+}
---
+++ GymAct/src/test/java/com/example/GymAct/GymActApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymAct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymActApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
